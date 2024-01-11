b54f8507b8490cf80689aaca27f80a1387e1194f
mrymahmdlo <ahmdlomrym@gmail.com>
2024-01-11 15:31:18 +0330

    route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { GeneralTaskComponent } from './general-task/general-task.component';
+import { TaskListComponent } from './task-list/task-list.component';
 
 const routes: Routes = [
   {
-    path: '07707563FQ8d6H1246GQN937c6c5bbc65cd',
+    path: '',
+    component: TaskListComponent,
+  },
+  {
+    path: '#/07707563FQ8d6H1246GQN937c6c5bbc65cd',
     component: GeneralTaskComponent,
   },
 ];
